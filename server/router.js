6a0b257d01f79a4acb34668d9bf2ef60ca2c4e5c
Matt Dickson <mdickson1737@gmail.com>
2017-02-21 10:54:13 -0500

    passport added

--- server/router.js
+++ server/router.js
@@ -1,9 +1,19 @@
 var Auth = require('./controllers/auth');
-var User = require('./models/user');
+var passportService = require('./services/passport');
+var passport = require('passport');
+// var User = require('./models/user');
 
+var requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){
 	
+	//test
+	app.get('/', requireAuth, function(req, res){
+		res.send('Hello Homepage');
+		//res.send({hi: 'there'});
+	});
+
+
 	//express.get  for getting data 
 	//express.post for posting data
 	//req    	   argument that represents an incoming http request ex. where its from 
