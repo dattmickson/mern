949a05cb62adea210abdd575a406e415457376a1
Matt Dickson <mdickson1737@gmail.com>
2017-02-22 09:48:38 -0500

    added embeded video

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react'; 
+import { Component } from 'react';
+import NavBarHeader from './nav';
+import Video from './video/video';
 
-	export default class App extends Component {
+export default class App extends Component {
 		render() {
 			return (
-					<div>React Hello World</div>
-				);
+  			<div>
+  				<NavBarHeader />
+  				<Video />
+			</div>
+			);
 		}
-	}
+}
