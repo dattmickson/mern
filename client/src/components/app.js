3d71cdb771a756ba2eaa9e0ad4ba4d3e0ed32d4e
Matt Dickson <mdickson1737@gmail.com>
2017-02-22 10:27:42 -0500

    module-12-forms

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import NavBarHeader from './nav';
 import Video from './video/video';
+import Signin from './auth/signin';
 
 export default class App extends Component {
 		render() {
@@ -9,6 +10,7 @@ export default class App extends Component {
   			<div>
   				<NavBarHeader />
   				<Video />
+  				{this.props.children}
 			</div>
 			);
 		}
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
+import Signin from './components/auth/signin';
 import App from './components/app';
 import reducers from './reducers';
 
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
   		<Router history={browserHistory}>
   			<Route path="/" component={App}>
+  				<Route path="signin" component={Signin} />
   			</Route>
   		</Router>
 </Provider>
