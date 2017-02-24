1096c4f3080692bed36f51f04b960ddd80ea3945
Matt Dickson <mdickson1737@gmail.com>
2017-02-24 09:49:07 -0500

    redux thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
+//const ROOT_URL = 'http://rest.learncode.academy/api/matt';
+const ROOT_URL = 'http://localhost:3000';
+
 export const CREATE_POSTS = 'CREATE_POSTS';
-
-const ROOT_URL = 'http://rest.learncode.academy/api/matt';
-
 export function createPost(props) {
 	const request = axios.post(`${ROOT_URL}/posts`, props);
 	return {
 		type: CREATE_POSTS,
 		payload: request
 	};
+}
+
+export function signinUser({ email, password }){
+	return function(dispatch){
+		axios.post(`${ROOT_URL}/signin`, {email, password})
+	}
 }
